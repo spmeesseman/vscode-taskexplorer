a4a3d2d5d02fc2d725776aa4b6e6bb340c351f9a
spmeesseman <spmeesseman@gmail.com>
2019-04-08 17:51:19 -0400

    test: bracn commit no launch ci

--- src/taskFolder.ts
+++ src/taskFolder.ts
@@ -3,10 +3,13 @@ import {
 	ThemeIcon, TreeItem, TreeItemCollapsibleState, WorkspaceFolder
 } from 'vscode';
 import { TaskFile } from './taskFile';
+import { timingSafeEqual } from 'crypto';
 
 export class TaskFolder extends TreeItem 
 {
 	public taskFiles: TaskFile[] = [];
+	public taskFolders: TaskFolder[] = [];
+
 	//public tasks: TaskItem[] = [];
 	public workspaceFolder: WorkspaceFolder;
 
@@ -20,5 +23,29 @@ export class TaskFolder extends TreeItem
 
 	addTaskFile(taskFile: TaskFile) {
 		this.taskFiles.push(taskFile);
+	}
+
+	addTaskFolder(taskFolder: TaskFolder) {
+		this.taskFolders.push(taskFolder);
+	}
+
+	removeTaskFile(taskFile: TaskFile) 
+	{
+		let idx: number = -1;
+		let idx2: number = -1;
+
+		this.taskFiles.forEach(each => {
+			idx++;console.log('4');
+			if (taskFile === each) {
+				idx2 = idx;console.log('5');
+			}
+		});
+
+		if (idx2 !== -1 && idx2 < this.taskFiles.length) {
+			console.log('removing task file ' + taskFile.label);
+			console.log('arrlen1 ' + this.taskFiles.length);
+			this.taskFiles.splice(idx2, 1);
+			console.log('arrlen2 ' + this.taskFiles.length);
+		}
 	}
 }
