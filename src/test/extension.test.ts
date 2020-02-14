24f89734c8eaf37df5936fa25d6a163aa5db19bd
spmeesseman <spmeesseman@gmail.com>
2020-02-14 06:27:03 -0500

    chore: running azure pipeline testing

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -28,45 +28,10 @@ suite("Extension Tests", () =>
     });
 
 
-        
-    // test("Enable required testing options", async function()
-    // {
-    //     //
-    //     // Enable views
-    //     //
-    //     await configuration.update('enableExplorerView', true);
-    //     await configuration.update('enableSideBar', true);
-    //     //
-    //     // Set misc settings
-    //     //
-    //     await configuration.update('includeAnt', ["**/test.xml", "**/emptytarget.xml", "**/emtyproject.xml"]);
-    //     await configuration.update('debug', true);
-    //     await configuration.update('debugLevel', 3);
-    //     //
-    //     // Enabled all options
-    //     //
-    //     await configuration.update('enableAnt', true);
-    //     await configuration.update('enableAppPublisher', true);
-    //     await configuration.update('enableBash', true);
-    //     await configuration.update('enableBatch', true);
-    //     await configuration.update('enableGradle', true);
-    //     await configuration.update('enableGrunt', true);
-    //     await configuration.update('enableGulp', true);
-    //     await configuration.update('enableMake', true);
-    //     await configuration.update('enableNpm', true);
-    //     await configuration.update('enableNsis', true);
-    //     await configuration.update('enablePowershell', true);
-    //     await configuration.update('enablePerl', true);
-    //     await configuration.update('enablePython', true);
-    //     await configuration.update('enableRuby', true);
-    //     await configuration.update('enableWorkspace', true);
-    // });
-
-
-    test("Activate extension", async function() 
+    test("Activate extension", function(done) 
     {
 
-        this.timeout(30 * 1000);
+        this.timeout(60 * 1000);
 
         const extension = vscode.extensions.getExtension(
             "spmeesseman.vscode-taskexplorer"
@@ -79,48 +44,90 @@ suite("Extension Tests", () =>
         //
         // Enable views
         //
-        await configuration.update('enableExplorerView', true);
-        await configuration.update('enableSideBar', true);
+        assert(configuration.update('enableExplorerView', true));
+        assert(configuration.update('enableSideBar', true));
         //
         // Set misc settings
         //
-        await configuration.update('includeAnt', ["**/test.xml", "**/emptytarget.xml", "**/emtyproject.xml"]);
-        await configuration.update('debug', true);
-        await configuration.update('debugLevel', 3);
+        assert(configuration.update('includeAnt', ["**/test.xml", "**/emptytarget.xml", "**/emtyproject.xml"]));
+        assert(configuration.update('debug', true));
+        assert(configuration.update('debugLevel', 3));
         //
         // Enable all task types
         //
-        await configuration.update('enableAnt', true);
-        await configuration.update('enableAppPublisher', true);
-        await configuration.update('enableBash', true);
-        await configuration.update('enableBatch', true);
-        await configuration.update('enableGradle', true);
-        await configuration.update('enableGrunt', true);
-        await configuration.update('enableGulp', true);
-        await configuration.update('enableMake', true);
-        await configuration.update('enableNpm', true);
-        await configuration.update('enableNsis', true);
-        await configuration.update('enablePowershell', true);
-        await configuration.update('enablePerl', true);
-        await configuration.update('enablePython', true);
-        await configuration.update('enableRuby', true);
-        await configuration.update('enableWorkspace', true);
+        assert(configuration.update('enableAnt', true));
+        assert(configuration.update('enableAppPublisher', true));
+        assert(configuration.update('enableBash', true));
+        assert(configuration.update('enableBatch', true));
+        assert(configuration.update('enableGradle', true));
+        assert(configuration.update('enableGrunt', true));
+        assert(configuration.update('enableGulp', true));
+        assert(configuration.update('enableMake', true));
+        assert(configuration.update('enableNpm', true));
+        assert(configuration.update('enableNsis', true));
+        assert(configuration.update('enablePowershell', true));
+        assert(configuration.update('enablePerl', true));
+        assert(configuration.update('enablePython', true));
+        assert(configuration.update('enableRuby', true));
+        assert(configuration.update('enableWorkspace', true));
 
-        if (!extension.isActive) 
+        configuration.update('includeAnt', ["**/test.xml", "**/emptytarget.xml", "**/emtyproject.xml"]).then(() =>
         {
-            console.log('        Manually activating extension');
-            try {
-                trees = await extension.activate();
+            if (!extension.isActive) 
+            {
+                console.log('        Manually activating extension');
+                extension.activate().then(
+                    api => {
+                        trees = api;
+                        assert(vscode.commands.executeCommand("taskExplorer.showOutput"));
+                        done();
+                    },
+                    () => {
+                        assert.fail("Failed to activate extension");
+                    }
+                );
+            } 
+            else {
                 assert(vscode.commands.executeCommand("taskExplorer.showOutput"));
+                done();
             }
-            catch(e) {
-                assert.fail("Failed to activate extension");
-            }
-        } 
-        else {
-            assert(vscode.commands.executeCommand("taskExplorer.showOutput"));
-        }
+        });
     });
+
+    
+    // test("Enable required testing options", async function()
+    // {
+    //     //
+    //     // Enable views
+    //     //
+    //     await trees.configuration.update('enableExplorerView', true);
+    //     await trees.configuration.update('enableSideBar', true);
+    //     //
+    //     // Set misc settings
+    //     //
+    //     await trees.configuration.update('includeAnt', ["**/test.xml", "**/emptytarget.xml", "**/emtyproject.xml"]);
+    //     await trees.configuration.update('debug', true);
+    //     await trees.configuration.update('debugLevel', 3);
+    //     //
+    //     // Enabled all options
+    //     //
+    //     await trees.configuration.update('enableAnt', true);
+    //     await trees.configuration.update('enableAppPublisher', true);
+    //     await trees.configuration.update('enableBash', true);
+    //     await trees.configuration.update('enableBatch', true);
+    //     await trees.configuration.update('enableGradle', true);
+    //     await trees.configuration.update('enableGrunt', true);
+    //     await trees.configuration.update('enableGulp', true);
+    //     await trees.configuration.update('enableMake', true);
+    //     await trees.configuration.update('enableNpm', true);
+    //     await trees.configuration.update('enableNsis', true);
+    //     await trees.configuration.update('enablePowershell', true);
+    //     await trees.configuration.update('enablePerl', true);
+    //     await trees.configuration.update('enablePython', true);
+    //     await trees.configuration.update('enableRuby', true);
+    //     await trees.configuration.update('enableWorkspace', true);
+    // });
+
 
     test("Check tree providers", function(done) 
     {
