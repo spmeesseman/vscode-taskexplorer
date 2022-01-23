1d4c1e2e3b8eb0ecc784f1272d932eb0f3255d69
Scott Meesseman <spmeesseman@gmail.com>
2022-01-23 03:16:13 -0500

    tests: ws settings file doesnt get removed on cleanup

--- src/test/suite/xDeactivate.test.ts
+++ src/test/suite/xDeactivate.test.ts
@@ -1,5 +1,6 @@
 
 import * as fs from "fs";
+import * as path from "path";
 import { deactivate } from "../../extension";
 import { activate, getWsPath } from "../helper";
 
@@ -18,7 +19,16 @@ suite("Deactivate Extension", () =>
 
 	test("Cleanup", async () =>
 	{
-		const dirNameCode = getWsPath(".vscode");
+		const dirNameCode = getWsPath(".vscode"),
+			  settingsFile = path.join(dirNameCode, "settings.json"),
+			  tasksFile = path.join(dirNameCode, "tasks.json");
+
+		if (fs.existsSync(settingsFile)) {
+			fs.unlinkSync(settingsFile);
+		}
+		if (fs.existsSync(tasksFile)) {
+			fs.unlinkSync(tasksFile);
+		}
 		if (fs.existsSync(dirNameCode)) {
 			fs.rmdirSync(dirNameCode, {
 				recursive: true
