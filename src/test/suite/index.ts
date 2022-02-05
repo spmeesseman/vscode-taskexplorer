2bfdee4f14f56a8947b41ad6bda5470c15d95152
Scott Meesseman <spmeesseman@gmail.com>
2022-02-04 20:14:11 -0500

    tests: update mocha color config (useColors bye bye)

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -86,7 +86,7 @@ export async function run(): Promise<void>
     //
     const mocha = new Mocha({
         ui: "tdd", // the TDD UI is being used in extension.test.ts (suite, test, etc.)
-        useColors: true, // colored output from test results,
+        color: true, // colored output from test results,
         timeout: 30000, // default timeout: 10 seconds
         retries: 1,
         reporter: "mocha-multi-reporters",
@@ -98,8 +98,6 @@ export async function run(): Promise<void>
             }
         }
     });
-
-    mocha.useColors(true);
 
     let filesToTest = "**/*.test.js";
     if (process.env.testArgs)
