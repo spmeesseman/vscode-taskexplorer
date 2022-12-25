bfb97fe02325c901a3c4be0bcbf52b9f10f50686
Scott Meesseman <spmeesseman@gmail.com>
2022-12-25 11:12:11 -0500

    chore: update types package

--- types/index.js
+++ types/index.js
@@ -10,5 +10,5 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./lib"), exports);
+__exportStar(require("./index"), exports);
 //# sourceMappingURL=index.js.map
