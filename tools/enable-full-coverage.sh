b7a92b404adc43df23eb4e38f0a81e3749310a02
Scott Meesseman <spmeesseman@gmail.com>
2023-01-06 21:22:57 -0500

    project:  trying to figure out good way to open log file in the active code instance after tests run

--- tools/enable-full-coverage.sh
+++ tools/enable-full-coverage.sh
@@ -8,6 +8,11 @@ if [ ! -z $1 ] ; then
     ENABLE=0
 fi
 
+LOGFILE=''
+if [ -z $2 ] ; then
+    LOGFILE=$3
+fi
+
 if [ $ENABLE = 1 ]; then
   #sed -i ':a;N;$!ba;s/"activationEvents": \[[ \r\n\t]*"onView:taskExplorer"/"activationEvents": []/g' package.json
   sed -i ':a;N;$!ba;s/"activationEvents": \[[ \r\n\t]*"\*"/"activationEvents": []/g' package.json
