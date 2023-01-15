823e3fb5a216298251c75f19fea11a981d704103
Scott Meesseman <spmeesseman@gmail.com>
2023-01-15 04:10:30 -0500

    fix: bug in proper casing when supposed to remove spaces (did ot matter until v3)

--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -459,7 +459,7 @@ export function properCase(name: string | undefined, removeSpaces?: boolean)
     if (!name) {
       return "";
     }
-    return name.replace(/(?:^\w|[A-Z]|\b\w)/g, (ltr) => ltr.toUpperCase()).replace(!removeSpaces ? /[ ]+/g : / /g, " ");
+    return name.replace(/(?:^\w|[A-Z]|\b\w)/g, (ltr) => ltr.toUpperCase()).replace(/[ ]+/g, !removeSpaces ? " " : "");
 }
 
 
