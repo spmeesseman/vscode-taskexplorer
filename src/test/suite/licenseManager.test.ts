fc7fc1ce46b4cd04f74c116c5e3fe44d68e42d3a
Scott Meesseman <spmeesseman@gmail.com>
2023-01-23 11:58:29 -0500

    tests: cont'd adjustments for updating to vscode/electron-test v2.2.2 and node v16

--- src/test/suite/licenseManager.test.ts
+++ src/test/suite/licenseManager.test.ts
@@ -120,7 +120,7 @@ suite("License Manager Tests", () =>
 	test("Get Maximum # of Tasks", async function()
 	{
         if (utils.exitRollingCount(this)) return;
-		this.slow(testControl.slowTime.licenseManagerDownCheck + (testControl.slowTime.storageSecretUpdate * 2));
+		this.slow(testControl.slowTime.storageSecretUpdate * 2);
 		await utils.setLicensed(true, licMgr);
 		expect(licMgr.getMaxNumberOfTasks()).to.be.a("number").that.is.equal(Infinity);
 		await utils.setLicensed(false, licMgr);
@@ -132,7 +132,7 @@ suite("License Manager Tests", () =>
 	test("Get Maximum # of NPM Tasks", async function()
 	{
         if (utils.exitRollingCount(this)) return;
-		this.slow(testControl.slowTime.licenseManagerDownCheck + (testControl.slowTime.storageSecretUpdate * 2));
+		this.slow(testControl.slowTime.storageSecretUpdate * 2);
 		await utils.setLicensed(true, licMgr);
 		expect(licMgr.getMaxNumberOfTasks("npm")).to.be.a("number").that.is.equal(Infinity);
 		await utils.setLicensed(false, licMgr);
@@ -156,7 +156,7 @@ suite("License Manager Tests", () =>
 	test("Get Maximum # of Bash Tasks (Scripts)", async function()
 	{
         if (utils.exitRollingCount(this)) return;
-		this.slow(testControl.slowTime.licenseManagerDownCheck + (testControl.slowTime.storageSecretUpdate * 2));
+		this.slow(testControl.slowTime.storageSecretUpdate * 2);
 		await utils.setLicensed(true, licMgr);
 		expect(licMgr.getMaxNumberOfTasks("bash")).to.be.a("number").that.is.equal(Infinity);
 		await utils.setLicensed(false, licMgr);
@@ -168,7 +168,7 @@ suite("License Manager Tests", () =>
 	test("Get Maximum # of Python Tasks (Scripts)", async function()
 	{
         if (utils.exitRollingCount(this)) return;
-		this.slow(testControl.slowTime.licenseManagerDownCheck + (testControl.slowTime.storageSecretUpdate * 2));
+		this.slow(testControl.slowTime.storageSecretUpdate * 2);
 		await utils.setLicensed(true, licMgr);
 		expect(licMgr.getMaxNumberOfTasks("python")).to.be.a("number").that.is.equal(Infinity);
 		await utils.setLicensed(false, licMgr);
@@ -180,7 +180,7 @@ suite("License Manager Tests", () =>
 	test("Get Maximum # of Task Files", async function()
 	{
         if (utils.exitRollingCount(this)) return;
-		this.slow(testControl.slowTime.licenseManagerDownCheck + (testControl.slowTime.storageSecretUpdate * 2));
+		this.slow(testControl.slowTime.storageSecretUpdate * 2);
 		await utils.setLicensed(true, licMgr);
 		expect(licMgr.getMaxNumberOfTaskFiles()).to.be.a("number").that.is.equal(Infinity);
 		await utils.setLicensed(false, licMgr);
