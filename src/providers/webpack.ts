dc60ff264aa55493b4fca36fc90a8ce040454b3c
Scott Meesseman <spmeesseman@gmail.com>
2023-01-31 09:09:03 -0500

    progress: add the cross-env version of webpack tasks

--- src/providers/webpack.ts
+++ src/providers/webpack.ts
@@ -14,12 +14,12 @@ export class WebpackTaskProvider extends TaskExplorerProvider implements TaskExp
     constructor() { super("webpack"); }
 
 
-    public createTask(target: string, cmd: "npx", folder: WorkspaceFolder, uri: Uri, xArgs: string[]): Task
+    public createTask(target: string, cmd: "webpack" |"cross-env", folder: WorkspaceFolder, uri: Uri, xArgs: string[]): Task
     {
         const def = this.getDefaultDefinition(target, folder, uri);
-        const args = [ "webpack", ...xArgs ];
+        const args = [ cmd, ...xArgs, "--config", "./" + def.fileName ];
         const options: ShellExecutionOptions = { cwd: dirname(uri.fsPath) };
-        const execution = new ShellExecution(cmd, args, options);
+        const execution = new ShellExecution("npx", args, options);
         return new Task(def, folder, target, "webpack", execution, "$webpackte");
     }
 
@@ -49,19 +49,25 @@ export class WebpackTaskProvider extends TaskExplorerProvider implements TaskExp
     {
         const tasks: Task[] = [],
               folder = workspace.getWorkspaceFolder(uri) as WorkspaceFolder,
-              groupSep = configuration.get<string>("groupSeparator"),
-              path = getRelativePath(folder, uri, true);
+              groupSep = configuration.get<string>("groupSeparator");
 
-        log.methodStart("read webpack uri task", 3, logPad, false, [[ "project folder", folder.name ], [ "relative path", path ]], this.logQueueId);
+        log.methodStart("read webpack uri tasks", 3, logPad, false, [[ "project folder", folder.name ], [ "path", uri.fsPath ]], this.logQueueId);
 
-        tasks.push(this.createTask(`build${groupSep}dev`, "npx", folder, uri, [ "--mode", "development", "--env", "environment=dev", "--config", "./" + path ]));
-        tasks.push(this.createTask(`build${groupSep}dev:verbose`, "npx", folder, uri, [ "--stats", "verbose", "--mode", "development", "--env", "environment=dev", "--config", "./" + path ]));
-        tasks.push(this.createTask(`build${groupSep}prod`, "npx", folder, uri, [ "--mode", "production", "--config", "./" + path ]));
-        tasks.push(this.createTask(`build${groupSep}prod:verbose`, "npx", folder, uri, [ "--stats", "verbose", "--mode", "production", "--config", "./" + path ]));
-        tasks.push(this.createTask(`build${groupSep}test`, "npx", folder, uri, [ "--mode", "development", "--env", "environment=test", "--config", "./" + path ]));
-        tasks.push(this.createTask(`rebuild${groupSep}dev`, "npx", folder, uri, [ "-w", "--mode", "development", "--env", "environment=dev", "--env", "clean=true", "--config", "./" + path ]));
-        tasks.push(this.createTask(`rebuild${groupSep}prod`, "npx", folder, uri, [ "--mode", "production", "--env", "clean=true", "--config", "./" + path ]));
-        tasks.push(this.createTask("watch", "npx", folder, uri, [ "-w", "--env", "environment=dev", "--config", "./" + path ]));
+        tasks.push(this.createTask(`build${groupSep}dev`, "webpack", folder, uri, [ "--mode", "development", "--env", "environment=dev" ]));
+        tasks.push(this.createTask(`build${groupSep}dev*verbose`, "webpack", folder, uri, [ "--stats", "verbose", "--mode", "development", "--env", "environment=dev" ]));
+        tasks.push(this.createTask(`build${groupSep}prod`, "webpack", folder, uri, [ "--mode", "production" ]));
+        tasks.push(this.createTask(`build${groupSep}prod*verbose`, "webpack", folder, uri, [ "--stats", "verbose", "--mode", "production" ]));
+        tasks.push(this.createTask(`build${groupSep}test`, "webpack", folder, uri, [ "--mode", "development", "--env", "environment=test" ]));
+        tasks.push(this.createTask(`rebuild${groupSep}dev`, "webpack", folder, uri, [ "--mode", "development", "--env", "environment=dev", "--env", "clean=true" ]));
+        tasks.push(this.createTask(`rebuild${groupSep}prod`, "webpack", folder, uri, [ "--mode", "production", "--env", "clean=true" ]));
+        tasks.push(this.createTask(`crossenv${groupSep}build${groupSep}dev`, "cross-env", folder, uri, [ "webpack", "--mode", "development", "--env", "environment=dev" ]));
+        tasks.push(this.createTask(`crossenv${groupSep}build${groupSep}dev*verbose`, "cross-env", folder, uri, [ "webpack", "--stats", "verbose", "--mode", "development", "--env", "environment=dev" ]));
+        tasks.push(this.createTask(`crossenv${groupSep}build${groupSep}prod`, "cross-env", folder, uri, [ "webpack", "--mode", "production" ]));
+        tasks.push(this.createTask(`crossenv${groupSep}build${groupSep}prod*verbose`, "cross-env", folder, uri, [ "webpack", "--stats", "verbose", "--mode", "production" ]));
+        tasks.push(this.createTask(`crossenv${groupSep}build${groupSep}test`, "cross-env", folder, uri, [ "webpack", "--mode", "development", "--env", "environment=test" ]));
+        tasks.push(this.createTask(`crossenv${groupSep}rebuild${groupSep}dev`, "cross-env", folder, uri, [ "webpack", "--mode", "development", "--env", "environment=dev", "--env", "clean=true" ]));
+        tasks.push(this.createTask(`crossenv${groupSep}rebuild${groupSep}prod`, "cross-env", folder, uri, [ "webpack", "--mode", "production", "--env", "clean=true" ]));
+        tasks.push(this.createTask("watch", "webpack", folder, uri, [ "-w", "--env", "environment=dev" ]));
 
         log.methodDone("read webpack uri tasks", 4, logPad, [[ "# of tasks found", tasks.length ]], this.logQueueId);
         return tasks;
