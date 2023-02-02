ab1ef4ce3d69a57d572113ae9416865dfda95f56
Scott Meesseman <spmeesseman@gmail.com>
2023-02-01 23:14:56 -0500

    build: add some webpack pluginsforanalyzing build and minimizing css (not working yet)

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,22 @@
 
 // const fs = require("fs");
 const path = require("path");
+const { renameSync } = require("fs");
+// const { IgnorePlugin } = require("webpack");
 // const ShebangPlugin = require("webpack-shebang-plugin");
 // const CopyWebpackPlugin = require("copy-webpack-plugin");
-const { renameSync } = require("fs");
 // const FilterWarningsPlugin = require("webpack-filter-warnings-plugin");
 // const TerserPlugin = require("terser-webpack-plugin");
-// const nodeExternals = require("webpack-node-externals");
-// const { IgnorePlugin } = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const CircularDependencyPlugin = require("circular-dependency-plugin");
+const CspHtmlPlugin = require("csp-html-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 /**
  * @typedef {Object} WebpackBuildEnv
  * @property {Boolean} clean If set,will clean the output directory before build
- * @property {String} environment The environment to compile for, one of `prod`, `dev`,
+ * @property {String} environment The environment to compile for, one of `prod`, `dev`, or `test`,
  * or `test`.  Note this is not the same as webpack `mode`.
  */
 /** @typedef {import("webpack").Configuration} WebpackConfig */
@@ -30,31 +34,35 @@ const { renameSync } = require("fs");
 module.exports = (env) =>
 {
 	if (!env) {
-		env = { clean: false, environment: "test" };
+		env = { clean: false, environment: "prod" };
 	}
 
-	if (env.clean?.toString() === "true") {
+	// @ts-ignore
+	if (env.clean === "true") {
 		env.clean = true;
 	}
 
+	// @ts-ignore
+	if (!env.target) {
+		// @ts-ignore
+		env.target = "node"; // or "webworker"
+	}
+
 	/**@type {WebpackConfig}*/
-	const wpConfig = {               // Base Webpack configuration object
-		target: "node",
-		// target: "webworker",
-		mode: env.environment !== "dev" && env.environment !== "test" ? "production" : "development",
-		resolve:
-		{   
-			extensions: [ ".ts", ".js" ],
-			// mainFields: [ "browser", "module", "main" ]
-		}
+	const wpConfig =  // Base Webpack configuration object
+	{   // @ts-ignore
+		target: env.target,
 	};
 
 	entryPoints(env, wpConfig);   // Entry points for built output
 	externals(wpConfig)           // External modules
 	optimization(env, wpConfig);  // Build optimization
+	mode(env, wpConfig);
 	output(env, wpConfig);        // Output specifications
 	plugins(env, wpConfig);       // Webpack plugins
+	resolve(wpConfig);            // Resolve config
 	rules(wpConfig);              // Loaders & other build rules
+	stats(wpConfig);              // Stats
 
 	return wpConfig;
 };
@@ -90,7 +98,7 @@ module.exports = (env) =>
 const devTool = (env, wpConfig) =>
 {
 	wpConfig.devtool = false;
-	if (env.environment === "dev" || wpConfig.mode === 'development')
+	if (env.environment === "dev" || wpConfig.mode === "development")
 	{
 		wpConfig.devtool = "source-map";
 	}
@@ -101,7 +109,7 @@ const devTool = (env, wpConfig) =>
 	if (!wpConfig.output) {
 		wpConfig.output = {};
 	}
-	wpConfig.output.devtoolModuleFilenameTemplate = '../[resource-path]'
+	wpConfig.output.devtoolModuleFilenameTemplate = "../[resource-path]"
 };
 
 
@@ -115,6 +123,7 @@ const devTool = (env, wpConfig) =>
  * @param {WebpackBuildEnv} env Webpack build environment
  * @param {WebpackConfig} wpConfig Webpack config object
  */
+// @ts-ignore
 const entryPoints = (env, wpConfig) =>
 {
 	wpConfig.entry = {
@@ -135,9 +144,9 @@ const externals = (wpConfig) =>
 	wpConfig.externals =
 	{   //
 		// the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot
-		// be webpack'ed, -> https://webpack.js.org/configuration/externals/
+		// be webpack"ed, -> https://webpack.js.org/configuration/externals/
 		//
-		vscode: 'commonjs vscode'
+		vscode: "commonjs vscode"
 	};
 };
 
@@ -206,7 +215,7 @@ const minification = (wpConfig) =>
 						output: null,
 						format: {},
 						// format: {       // Default {}
-						// 	comments: false, // default 'some'
+						// 	comments: false, // default "some"
 						// 	shebang: true
 						// },
 						// toplevel (default false) - set to true to enable top level variable
@@ -222,6 +231,29 @@ const minification = (wpConfig) =>
 				})
 			]
 		});*/
+	}
+};
+
+
+//
+// *************************************************************
+// *** MODE                                                  ***
+// *************************************************************
+//
+/**
+ * @method
+ * @param {WebpackBuildEnv} env Webpack build environment
+ * @param {WebpackConfig} wpConfig Webpack config object
+ */
+const mode = (env, wpConfig) =>
+{
+	if (env.environment === "dev" || env.environment === "test") {
+		wpConfig.mode = "development";
+	}
+	else //
+	{   // env.environment === "prod"
+		wpConfig.mode = "production";
+		env.environment = "prod";
 	}
 };
 
@@ -289,8 +321,10 @@ const output = (env, wpConfig) =>
  * @param {WebpackConfig} wpConfig Webpack config object
  */
 const plugins = (env, wpConfig) =>
-{
+{   // @ts-ignore
+	const imageGeneratorConfig = getImageMinimizerConfig(wpConfig.mode, env);
 	wpConfig.plugins = [];
+
 	if (wpConfig.mode === "production")
 	{
 		wpConfig.plugins = [ // add AfterDone plugin at the end of the plugins array
@@ -305,8 +339,57 @@ const plugins = (env, wpConfig) =>
 					} catch {}
 				});
 			}
-		}];
+		},
+		new ImageMinimizerPlugin({
+			deleteOriginalAssets: true,
+			generator: [imageGeneratorConfig],
+		}),
+		new CssMinimizerPlugin({
+			minimizerOptions: {
+				preset: [
+					"cssnano-preset-advanced",
+					{ discardUnused: false, mergeIdents: false, reduceIdents: false },
+				],
+			},
+		})];
+		// @ts-ignore
+		if (env.analyze === "true")
+		{
+			wpConfig.plugins.push(new BundleAnalyzerPlugin({ analyzerPort: "auto" }));
+
+			wpConfig.plugins.push(
+				new CircularDependencyPlugin(
+				{
+					cwd: __dirname,
+					exclude: /node_modules/,
+					failOnError: false,
+					onDetected: function ({ module: _webpackModuleRecord, paths, compilation })
+					{   // @ts-ignore
+						compilation.warnings.push(new WebpackError(paths.join(" -> ")));
+					},
+				}),
+			);
+		}
 	}
+};
+
+
+//
+// *************************************************************
+// *** RESOLVE                                               ***
+// *************************************************************
+//
+/**
+ * @method
+ * @param {WebpackConfig} wpConfig Webpack config object
+ */
+const resolve = (wpConfig) =>
+{
+	wpConfig.resolve =
+	{   
+		extensions: [ ".ts", ".js" ],
+		// mainFields: [ "browser", "module", "main" ]
+	};
 };
 
 
@@ -326,7 +409,7 @@ const rules = (wpConfig) =>
 			test: /\.ts$/,
 			exclude: [/node_modules/, /test/],
 			use: [{
-				loader: 'ts-loader'
+				loader: "ts-loader"
 			}]
 		},
 		// {
@@ -335,11 +418,126 @@ const rules = (wpConfig) =>
 		// },
 		// {
 		// 	exclude: [ /.(js|jsx|mjs|html|json)$/],
-		// 	loader: 'file-loader',
+		// 	loader: "file-loader",
 		// 	options: {
 		// 		// name: config.mediaChunkFileName
 		// 	},
 		// }
 		]
+	};
+};
+
+
+//
+// *************************************************************
+// *** CSP                                                   ***
+// *************************************************************
+//
+/**
+ * @param { "production" | "development" | "none" } mode
+ * @param {{ analyzeBundle?: boolean; analyzeDeps?: boolean; esbuild?: boolean; useSharpForImageOptimization?: boolean } | undefined } env
+ * @returns { CspHtmlPlugin }
+ */
+// @ts-ignore
+const getCspHtmlPlugin = (mode, env) =>
+{
+	const cspPlugin = new CspHtmlPlugin(
+	{
+		'default-src': "'none'",
+		'img-src': ['#{cspSource}', 'https:', 'data:'],
+		'script-src':
+			mode !== 'production'
+				? ['#{cspSource}', "'nonce-#{cspNonce}'", "'unsafe-eval'"]
+				: ['#{cspSource}', "'nonce-#{cspNonce}'"],
+		'style-src':
+			mode === 'production'
+				? ['#{cspSource}', "'nonce-#{cspNonce}'", "'unsafe-hashes'"]
+				: ['#{cspSource}', "'unsafe-hashes'", "'unsafe-inline'"],
+		'font-src': ['#{cspSource}'],
+	},
+	{
+		enabled: true,
+		hashingMethod: 'sha256',
+		hashEnabled: {
+			'script-src': true,
+			'style-src': mode === 'production',
+		},
+		nonceEnabled: {
+			'script-src': true,
+			'style-src': mode === 'production',
+		},
+	});
+	// Override the nonce creation so we can dynamically generate them at runtime
+	// @ts-ignore
+	cspPlugin.createNonce = () => '#{cspNonce}';
+	return cspPlugin;
+};
+
+
+//
+// *************************************************************
+// *** IMAGES                                                ***
+// *************************************************************
+//
+/**
+ * @param {'production'|'development'|'none'|'tests'} mode
+ * @param {{ analyzeBundle?: boolean; analyzeDeps?: boolean; esbuild?: boolean; useSharpForImages?: boolean } | undefined } env
+ * @returns { ImageMinimizerPlugin.Generator<any> }
+ */
+const getImageMinimizerConfig = (mode, env) =>
+{
+	/** @type ImageMinimizerPlugin.Generator<any> */
+	// @ts-ignore
+	return env.useSharpForImages ?
+	{
+		type: "asset",
+		implementation: ImageMinimizerPlugin.sharpGenerate,
+		options: {
+			encodeOptions: {
+				webp: {
+					lossless: true,
+				},
+			},
+		},
+	} :
+	{
+		type: "asset",
+		implementation: ImageMinimizerPlugin.imageminGenerate,
+		options: {
+			plugins: [
+			[
+				"imagemin-webp",
+				{
+					lossless: true,
+					nearLossless: 0,
+					quality: 100,
+					method: mode === "production" ? 4 : 0,
+				},
+			]]
+		}
+	};
+};
+
+
+//
+// *************************************************************
+// *** STATS                                                 ***
+// *************************************************************
+//
+/**
+ * @method
+ * @param {WebpackConfig} wpConfig Webpack config object
+ */
+// @ts-ignore
+const stats = (wpConfig) =>
+{
+	wpConfig.stats = {
+		preset: "errors-warnings",
+		assets: true,
+		colors: true,
+		env: true,
+		errorsCount: true,
+		warningsCount: true,
+		timings: true
 	};
 };
