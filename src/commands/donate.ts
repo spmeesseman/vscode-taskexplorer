318d01203621034a5d7796dadd6db7e170cee905
Scott Meesseman <spmeesseman@gmail.com>
2023-02-08 05:43:41 -0500

    chore: add icons and nls entry for donate command

--- src/commands/donate.ts
+++ src/commands/donate.ts
@@ -1,6 +1,6 @@
 
 import log from "../lib/log/log";
-import { ExtensionContext } from "vscode";
+import { ExtensionContext, window } from "vscode";
 import { Commands } from "../lib/constants";
 import { registerCommand } from "../lib/command";
 
@@ -8,6 +8,7 @@ import { registerCommand } from "../lib/command";
 const donate = async() =>
 {
     log.methodStart("donate command", 1, "", true);
+    window.showInformationMessage("Not implemented yet");
     log.methodDone("donate command", 1, "");
 };
 
