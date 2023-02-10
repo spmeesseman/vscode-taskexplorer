60204de64a45a55378fc7dc2c3af73b59cc421fe
Scott Meesseman <spmeesseman@gmail.com>
2023-02-10 14:39:45 -0500

    progress: conversion of all webviews to WebviewApp

--- src/webview/app/releaseNotes/releaseNotes.ts
+++ src/webview/app/releaseNotes/releaseNotes.ts
@@ -3,6 +3,7 @@ import "./release-notes.css";
 import "../common/css/fa.css";
 import { State } from "../../common/state";
 import { TeWebviewApp } from "../webviewApp";
+import { Disposable, DOM } from "../common/dom";
 
 
 export class ReleaseNotesWebviewApp extends TeWebviewApp<State>
@@ -11,6 +12,27 @@ export class ReleaseNotesWebviewApp extends TeWebviewApp<State>
     {
 		super("ReleaseNotesWebviewApp");
 	}
+
+
+    protected override onBind(): Disposable[]
+    {
+		const disposables = super.onBind?.() ?? [];
+		disposables.push(
+			DOM.on("btnViewReleaseNotes", "click", (_e: MouseEvent, element: HTMLElement) => this.toggle(element)),
+		);
+		return disposables;
+    }
+
+
+	private toggle = (element: HTMLElement) =>
+	{
+		const x = document.getElementById("releaseNotesDiv");
+		const showing = element.classList.toggle("is-show");
+		element.classList.remove(!showing ? "fa-chevron-down" : "fa-chevron-up");
+		element.classList.add(!showing ? "fa-chevron-up" : "fa-chevron-down");
+	};
+
 }
+
 
 new ReleaseNotesWebviewApp();
